b6b88a7dc4f01edefb1b8489a3a9655fbe874591
Seungmin-Yang <tmdals099@gmail.com>
2019-09-25 20:41:33 +0900

    New project
    Created new project

---
+++ app/src/main/java/com/yangseungmin/bcsdlab_exercise_widget/MainActivity.java
@@ -0,0 +1,14 @@
+package com.yangseungmin.bcsdlab_exercise_widget;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
