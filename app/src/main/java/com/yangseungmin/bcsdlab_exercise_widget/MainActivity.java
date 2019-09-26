f83d2d10bf39f0e3dea11bf062f1ed1f22eb2b4a
Seungmin-Yang <tmdals099@gmail.com>
2019-09-26 11:07:56 +0900

    First toast feature

--- app/src/main/java/com/yangseungmin/bcsdlab_exercise_widget/MainActivity.java
+++ app/src/main/java/com/yangseungmin/bcsdlab_exercise_widget/MainActivity.java
@@ -3,8 +3,13 @@ package com.yangseungmin.bcsdlab_exercise_widget;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button toastButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +20,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void init() {
+        toastButton = (Button) findViewById(R.id.button_toast);
+        toastButton.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_toast:
+                Toast.makeText(this, "Toast Example", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
